b23bc7008c2bbc0f965c7efb2ac7e221aec00496
Alberto Martinez <j.alberto.martinez.d@gmail.com>
2021-03-09 17:15:51 -0600

    Create data classes and DTOs, also included automapper and change appsettings file

--- BookStore-API/Data/ApplicationDbContext.cs
+++ BookStore-API/Data/ApplicationDbContext.cs
@@ -12,5 +12,8 @@ namespace BookStore_API.Data
             : base(options)
         {
         }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
